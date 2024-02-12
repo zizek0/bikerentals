19e0f8cb19d4fd72c7bdc0762db93ba3dbd9470a
zizek0 <dummyac707@gmail.com>
2024-02-12 14:57:43 +0530

    fix

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,14 +1,17 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import InputItem from "../Booking/InputItem";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
 import "leaflet-defaulticon-compatibility";
+import { relative } from "path";
+
 function Map() {
   const [selectedCity, setSelectedCity] = useState("");
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+  const mapRef = useRef<MapContainer>(null);
 
   const handleCitySelect = (city: string, lat: number, lon: number) => {
     setSelectedCity(city);
@@ -17,29 +20,26 @@ function Map() {
   };
 
   useEffect(() => {
-    // Re-render the map when latitude or longitude changes
-    // You can add any additional logic here if needed
+    if (mapRef.current) {
+      mapRef.current.flyTo([latitude, longitude], 14);
+    }
   }, [latitude, longitude]);
 
   return (
-    <div
-      className="p-4 md:p-5*
-    border-[2px]"
-    >
+    <div className="p-4 md:p-5 border-[2px]">
       <p className="text-[20px] font-bold">📍 Bikes around you</p>
       <InputItem toggleState={handleCitySelect} />
-      <p>
-        {selectedCity} {latitude} {longitude}
-      </p>
+      
 
       <MapContainer
+        ref={mapRef}
         style={{
           height: "70vh",
-          
+          position: "relative",
+          margin: "0.8rem 0 0 0",
         }}
-        center={[15.29932650, 74.12399600]}
+        center={[15.2993265, 74.123996]}
         zoom={10}
-        scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
