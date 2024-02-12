0162031a640e78368e50b218edf732a27b34e389
my-project-page <dummyac707@gmail.com>
2024-02-12 11:58:58 +0530

    map coordinates fix

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -37,8 +37,8 @@ function Map() {
           height: "70vh",
           
         }}
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={[15.29932650, 74.12399600]}
+        zoom={10}
         scrollWheelZoom={false}
       >
         <TileLayer
